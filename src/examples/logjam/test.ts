9eb64b66846a268d240dc4a01f0827e5ffa4e263
lucaliebenberg <89966720+lucaliebenberg@users.noreply.github.com>
2024-07-16 20:14:38 +0200

    work on's for mocking and spying

--- src/examples/logjam/test.ts
+++ src/examples/logjam/test.ts
@@ -1,3 +1,13 @@
 import { test, expect, vi } from 'vitest';
 
-test.todo('it spies on the multiply method');
+test.todo('it spies on the multiply method', () => {
+
+    vi.mock('hi')
+
+    vi.mock('node-fetch', () => {
+        return {
+            // some JSON response
+        }
+    })
+
+});
